c6ad857e3eb54b51dfe92ce728ba9389d6b0470b
kakunge <kakunge3@naver.com>
2023-10-04 15:20:25 +0900

    Update /cmdi

--- app.js
+++ app.js
@@ -6,7 +6,24 @@ const app = express();
 const port = 80;
 
 app.use(express.json());
+app.use(cors());
 app.use(express.static('public'));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'dummy'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error("MySQL connection error : " + err.stack);
+        return;
+    }
+
+    console.log("MySQL connection success" + connection.threadId);
+})
 
 app.get('/', (req, res) => {
     res.send("Hi, this is a dummy web!</br><a href='/cmdi'>Go to command page</a>");
@@ -19,17 +36,22 @@ app.get('/cmdi', (req, res) => {
 app.post('/cmdi', (req, res) => {
     const command = req.body.command;
 
+    console.log("POST");
+
     exec(command, (error, stdout, stderr) => {
         if (error) {
+            console.error("Error occurred : ", error);
             res.send("Error occurred : " + error.message);
             return;
         }
         if (stderr) {
+            console.error("CLI error : ", stderr);
             res.send("CLI error : " + stderr);
             return;
         }
 
         res.send("Result : " + stdout);
+        console.log(stdout);
     })
 })
 
